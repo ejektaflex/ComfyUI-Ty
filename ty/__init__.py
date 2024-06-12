0bda7935992b09421f3f6365149cb4906e00ff08
E F <dootydooter@gmail.com>
2024-06-12 11:08:12 -0500

    Rework to support more nodes

---
+++ ty/__init__.py
@@ -0,0 +1,8 @@
+from .regex_weighter import LoraBlockRegexLoader
+
+NODE_CLASS_MAPPINGS = {
+    "Lora Block Weight Regex Loader // Ty": LoraBlockRegexLoader
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Lora Block Weight Regex Loader // Ty": "Lora Block Weight Regex Loader"
+}
--- ty/regex_weighter.py
+++ ty/regex_weighter.py
@@ -92,9 +92,4 @@ class LoraBlockRegexLoader:
 
 
 
-NODE_CLASS_MAPPINGS = {
-    "Lora Block Weight Regex Loader // Ty": LoraBlockRegexLoader
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Lora Block Weight Regex Loader // Ty": "Lora Block Weight Regex Loader"
-}
+
